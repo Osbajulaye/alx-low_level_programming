04e708b577f55b3716d42e129e828fc05d47d409
Olakunle Bajulaye <osbajulaye@gmail.com>
2023-08-10 09:07:03 -0700

    hover

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 	
-int main(void) /*using printf to diplay strings*/
+int main(void) // display strings
 
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n"); // print statement
 	return (0);
 }
 
