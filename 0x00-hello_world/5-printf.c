3747c91968ae1a801c720eed65be046ad05772b3
Olakunle Bajulaye <osbajulaye@gmail.com>
2023-08-10 09:03:05 -0700

    good luck

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 	
-int main(){
+int main(void) /*using printf to diplay strings*/
+
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
 
