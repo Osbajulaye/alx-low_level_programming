b3b60ffb3de8e0aac66b89a0782e6600fd55fae0
Olakunle Bajulaye <osbajulaye@gmail.com>
2023-08-10 15:59:09 -0700

    goody

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 # include  <stdio.h>
 /* Display string using puts commands*/
 
-int main(void) {
-	/* main function */
+int main(void) /* main function */
+{
 
 	puts("\"Programming is like building a multilingual puzzle"); /* input command */
 
