60d3f9df4c07a8e2648ff425a7eaef1cd389d472
Olakunle Bajulaye <osbajulaye@gmail.com>
2023-08-23 18:59:27 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+char *leet(char *n)
+{
+	int i;
+	int a;
+	char b[11] = "aAeEoOtTlL";
+	charc[11] = "4433007711";
+
+	for(i = 0; n[i] != '\0'; i++)
+	{
+		for(a = 0; a <= 9; a++)
+		{
+			if(b[a] == n[i])
+			{
+			}
+		}
+	}
+	return (n);
+}
