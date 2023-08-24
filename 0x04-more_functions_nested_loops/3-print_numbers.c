b1001b2ce3ba57170f2f1f403b4e438db4d69bbb
Osbajulaye <osbajulaye@gmail.com>
2023-08-24 16:07:23 +0000

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print numbers- print 0-9
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		putchar(i + 48);
+	putchar('\n');
+}
