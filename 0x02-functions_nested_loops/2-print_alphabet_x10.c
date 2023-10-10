a80cc43e51508951858281392eba2f8a4edcdcfb
Osbajulaye <osbajulaye@gmail.com>
2023-10-10 13:32:51 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,13 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+		i++;
 		_putchar('\n');
 	}
 }
