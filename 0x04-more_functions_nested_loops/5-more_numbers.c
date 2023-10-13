d47961d65b77e0041d19c1667f6760538d6aff9c
Osbajulaye <osbajulaye@gmail.com>
2023-10-13 23:18:11 +0000

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 	int n;
 	for (count = 0; count < 10; count++)
 	{
-		for (n =0; n <=14; n++)
+		for (n =0; n <= 14; n++)
 		{
 			if (n > 9)
 			{
-				_putchar((n/10) + '0');
+				_putchar((n / 10) + '0');
 			}
-			_putchar((n%10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
