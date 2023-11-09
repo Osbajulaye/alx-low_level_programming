393d0cf1bf9ff622285e1a003bcb829f043aac63
Osbajulaye <osbajulaye@gmail.com>
2023-11-09 20:14:03 +0000

    Corrections

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,12 @@
 #include <stddef.h>
 
 int _putchar(char c);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
